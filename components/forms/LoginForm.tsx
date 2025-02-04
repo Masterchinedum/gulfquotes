fbda31fa43dccac319e4af1c5152a6256fb58f5b
Masterchinedum <chinedu02k@gmail.com>
2025-02-04 08:48:21 -0800

    feat: update redirect path in LoginForm to root for improved user experience

--- components/forms/LoginForm.tsx
+++ components/forms/LoginForm.tsx
@@ -71,7 +71,7 @@ export function LoginForm() {
                 action={async () => {
                   "use server";
                   await signIn("github", {
-                    redirectTo: "/onboarding",
+                    redirectTo: "/",
                   });
                 }}
               >
@@ -86,7 +86,7 @@ export function LoginForm() {
                 action={async () => {
                   "use server";
                   await signIn("google", {
-                    redirectTo: "/onboarding",
+                    redirectTo: "/",
                   });
                 }}
               >
