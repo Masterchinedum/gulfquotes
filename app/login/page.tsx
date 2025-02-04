113943367949fefdcc5f66fa227a0623dea82c4c
Masterchinedum <chinedu02k@gmail.com>
2025-02-04 07:10:42 -0800

    feat: update branding in LoginPage and add GeneralSubmitButton component for enhanced form submission

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -11,11 +11,11 @@ const LoginPage = () => {
         <Link href="/" className="flex items-center gap-2 self-center">
           <Image src={Logo} alt="Logo" className="size-10" />
           <h1 className="text-2xl font-bold">
-            Job<span className="text-primary">Marshal</span>
+            Quote<span className="text-primary">Icon</span>
           </h1>
         </Link>
         <LoginForm />
-      </div>
+      </div> 
     </div>
   );
 };
