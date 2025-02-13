0d817a77adf49d828a100976c70999dd6aca75df
Masterchinedum <chinedu02k@gmail.com>
2025-02-12 21:59:20 -0800

    fix: update cookie header handling in fetchUserProfile function

--- actions/user-profile.ts
+++ actions/user-profile.ts
@@ -4,12 +4,13 @@ import { UserResponse } from "@/types/api/users";
 
 export async function fetchUserProfile(slug: string): Promise<UserResponse> {
   try {
+    const headersList = await headers();
     const origin = process.env.NEXTAUTH_URL || "";
     const res = await fetch(`${origin}/api/users/${slug}`, {
       headers: {
-        cookie: headers().get("cookie") || "",
-      },
-      cache: "no-store",
+          cookie: headersList.get("cookie") || "",
+          },
+          cache: "no-store",
     });
 
     const result: UserResponse = await res.json();
