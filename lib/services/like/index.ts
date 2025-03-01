002ec777b03b6a5568df76351ef2e74549354768
Masterchinedum <chinedu02k@gmail.com>
2025-03-01 06:57:43 +0100

    feat: create index file for like services to streamline imports

---
+++ lib/services/like/index.ts
@@ -0,0 +1,4 @@
+// lib/services/like/index.ts
+export { default as quoteLikeService } from './quote-like.service';
+export { default as commentLikeService } from './comment-like.service';
+export { default as replyLikeService } from './reply-like.service';
