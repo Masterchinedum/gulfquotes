212afb532a357c2e2c4dba8c9286be29744f4a31
Masterchinedum <chinedu02k@gmail.com>
2025-03-01 19:55:13 +0100

    feat: add bookmark service export for quote bookmarks

---
+++ lib/services/bookmark/index.ts
@@ -0,0 +1,6 @@
+// Export the bookmark service
+export { quoteBookmarkService } from './quote-bookmark.service';
+
+// Default export for compatibility with both import styles
+import { quoteBookmarkService } from './quote-bookmark.service';
+export default quoteBookmarkService;
